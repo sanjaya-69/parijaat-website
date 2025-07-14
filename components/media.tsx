f70784050db849e1cd45b4700e055437827b1678
Sanjaya Regmi <regmisanjaya1@gmail.com>
2025-07-14 14:38:47 +0545

    new photos

--- components/media.tsx
+++ components/media.tsx
@@ -13,7 +13,7 @@ export default function Media() {
      {
       type: "gallery",
       title: "Board Member",
-      thumbnail: "/image7.jpg?height=200&width=300",
+      thumbnail: "/image5.jpg?height=200&width=300",
       icon: <FileText className="h-8 w-8 text-white" />,
     },
     {
